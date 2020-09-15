cfae4514a45cb907b6b548ba1081bd6a401e2299
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2020-09-15 19:32:04 +0530

    Added how to IQueryable

---
+++ IQueryable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class IQueryableInterface
+    {
+
+        private static void UsedOfIqeryable()
+        {
+            List<int> myList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            IQueryable<int> methodSyntax = myList.AsQueryable().Where(em => em > 5);
+            foreach (int ele in methodSyntax)
+            {
+                Console.WriteLine(ele);
+            }            
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Used of IQueryable interface");
+            UsedOfIqeryable();
+            Console.ReadKey();
+        }
+    }
+}
